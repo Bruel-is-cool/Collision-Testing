7ee93caec432fda51efe515be7a3f56ceec7aefb
Bruel-is-cool <benbruel@outlook.com>
2024-12-03 16:12:32 +0000

    pro gamer moves
    oh yeah

--- Assets/Scripts/RigidbodyMovement.cs
+++ Assets/Scripts/RigidbodyMovement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]       //We will still need to tweak some of the settings.
 public class RigidbodyMovement : MonoBehaviour
 {
+    Animator animator;
     Rigidbody2D rb2d;
     public float moveSpeed = 5f;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
     }
 
@@ -16,6 +18,9 @@ public class RigidbodyMovement : MonoBehaviour
     {
         float moveInputX = Input.GetAxisRaw("Horizontal"); // For horizontal movement (left/right)
         float moveInputY = Input.GetAxisRaw("Vertical");   // For vertical movement (up/down)
+
+        animator.SetFloat("InputX", moveInputX);
+        animator.SetFloat("InputY", moveInputY);
 
         // Normalise the vector so that we don't move faster when moving diagonally.
         Vector2 moveDirection = new Vector2(moveInputX, moveInputY);
